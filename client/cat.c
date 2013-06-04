b8d7c0562ca766459ef6d72a89881e5d369e257f
Emilio López <elopez93@gmail.com>
2013-06-03 21:26:16 -0300

    client: fix buffer overflow on cat

--- client/cat.c
+++ client/cat.c
@@ -5,15 +5,15 @@ int main(int argc, char **argv)
 {
 	FD src;
 	int file = 1;
-	char c;
+	char c[2];
 	if (argc < 2)
 		return -1;
 	if (dfs_connect("127.0.0.1") == -1)	// Conectarse al servidor
 		return -1;
 	while (file < argc) {
 		src = dfs_open(argv[file]);	// Abrir el archivo origen
-		while (dfs_read(src, 1, &c) != EOF) {
-			putchar(c);
+		while (dfs_read(src, 1, c) != EOF) {
+			putchar(*c);
 		}
 		dfs_close(src);
 		file++;
