16cddcf2a01bf5cda6af90226e4de85b9b24e46c
Emilio López <elopez93@gmail.com>
2014-12-06 22:51:04 -0300

    server: pthreads: better runworkers.sh

--- server/pthreads/runworkers.sh
+++ server/pthreads/runworkers.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
+WORKER_PIDS=""
+
 for PORT in 8100 8200 8300 8400 8500; do
 	./worker $PORT 127.0.0.1:8100 127.0.0.1:8200 127.0.0.1:8300 127.0.0.1:8400 127.0.0.1:8500 &
+	WORKER_PIDS="$! $WORKER_PIDS"
 done
 sleep 1
+
+export WORKER_PIDS
+trap "kill -- $WORKER_PIDS" INT TERM EXIT
+
 ./dispatcher 127.0.0.1:8100 127.0.0.1:8200 127.0.0.1:8300 127.0.0.1:8400 127.0.0.1:8500
-killall worker valgrind dispatcher
